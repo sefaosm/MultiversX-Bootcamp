5e7f4caac25e5146903d8344a7f13a9ccd4dc5f3
sefaosm <sfsmnl@gmail.com>
2024-12-16 02:46:45 +0300

    first commit

---
+++ src/bin/concatenate-strings.rs
@@ -0,0 +1,15 @@
+fn concatenate_strings(str1: &str, str2: &str) -> String {
+    let mut result = String::new();
+    result.push_str(str1);
+    result.push_str(str2);
+    result
+}
+
+fn main() {
+    let str1 = String::from("Hello,");
+    let str2 = String::from(" World!");
+
+    let concatenated_string = concatenate_strings(&str1, &str2);
+
+    println!("Concatenated string: {}", concatenated_string);
+}
